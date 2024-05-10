d15bd5eabbf8403077d8fed87266bbb056b69ab4
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-10 17:30:19 +0300

    template<typeneme>

--- STL_Templates.cpp
+++ STL_Templates.cpp
@@ -1,9 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
+int addInt(int a, int b) {
+    return a + b;
+}
+
+float addFloat(float a, float b) {
+    return a + b;
+}
+
+template < typename T>
+T addFloat(T a, T b) {
+    return a + b;
+}
 
 int main()
 {
-    
-
+    cout << addInt(5, 7) << endl;
+    cout << addFloat(5.3, 7.7) << endl;
     cin.get();
 }
