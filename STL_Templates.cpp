60d699344dc3ae25fea10c56b22d93bcee326f59
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2024-05-10 17:50:33 +0300

    add input

--- STL_Templates.cpp
+++ STL_Templates.cpp
@@ -8,8 +8,8 @@ float addFloat(float a, float b) {
     return a + b;
 }
 
-template < typename T>
-T addFloat(T a, T b) {
+template < typename T> //имя типа"Т"
+T add(T a, T b) {      //универсальная шаблонная ф--ция
     return a + b;
 }
 
@@ -17,5 +17,7 @@ int main()
 {
     cout << addInt(5, 7) << endl;
     cout << addFloat(5.3, 7.7) << endl;
+    cout << add(5, 7) << endl;        //вывод шаблонной ф--ции int
+    cout << add(5.4, 7.6) << endl;    //вывод шаблонной ф--ции float
     cin.get();
 }
